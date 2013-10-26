1bc973f488078e35ce7a061c87fd6ec0788591f9
Mara Bos <m-ou.se@m-ou.se>
2013-10-26 18:53:32 +0200

    Remove moggle.core.gl more lightweight and drop unchecked_gl.

--- moggle/core/gl.d
+++ moggle/core/gl.d
@@ -2,9 +2,22 @@ module moggle.core.gl;
 
 import std.traits;
 
-import moggle.core.unchecked_gl;
+import derelict.opengl3.gl3;
+public import derelict.opengl3.types;
+public import derelict.opengl3.constants;
 
-public import moggle.core.unchecked_gl : loadOpenGL;
+pragma(lib, "dl");
+pragma(lib, "DerelictGL3");
+pragma(lib, "DerelictUtil");
+
+static this() {
+	DerelictGL3.load();
+}
+
+/// Call this after creating your OpenGL context.
+GLVersion loadOpenGL() {
+	return DerelictGL3.reload();
+}
 
 class GlError : Exception {
 	this(string func, string what, string file = __FILE__, size_t line = __LINE__) {
@@ -38,18 +51,17 @@ debug {
 
 	mixin((){
 		string code;
-		foreach (x; [
-			__traits(allMembers, derelict.opengl3.functions),
-			__traits(allMembers, derelict.opengl3.constants),
-			__traits(allMembers, derelict.opengl3.types),
-			__traits(allMembers, derelict.opengl3.arb),
-			__traits(allMembers, derelict.opengl3.ext),
-			__traits(allMembers, moggle.core.unchecked_gl)
+		foreach (part, members; [
+			"functions": [__traits(allMembers, derelict.opengl3.functions)],
+			"arb": [__traits(allMembers, derelict.opengl3.arb)],
+			"ext": [__traits(allMembers, derelict.opengl3.ext)]
 		]) {
-			if (x[0..2] == "gl" && x != "glGetError") {
-				code ~= "alias wrap!(moggle.core.unchecked_gl." ~ x ~ ") " ~ x ~ ";\n";
-			} else if (x[0..2] == "GL" || x == "glGetError") {
-				code ~= "alias moggle.core.unchecked_gl." ~ x ~ " " ~ x ~ ";\n";
+			foreach (x; members) {
+				if (x[0..2] == "gl" && x != "glGetError") {
+					code ~= "alias wrap!(derelict.opengl3." ~ part ~ "." ~ x ~ ") " ~ x ~ ";\n";
+				} else if (x[0..2] == "GL" || x == "glGetError") {
+					code ~= "alias derelict.opengl3." ~ part ~ "." ~ x ~ " " ~ x ~ ";\n";
+				}
 			}
 		}
 		return code;
@@ -57,7 +69,19 @@ debug {
 
 } else {
 
-	public import moggle.core.unchecked_gl;
+	public import derelict.opengl3.functions;
+	public import derelict.opengl3.arb;
+	public import derelict.opengl3.ext;
 
 }
 
+template GL_type(T) {
+	     static if (is(T == GLfloat )) alias GL_FLOAT          GL_type;
+	else static if (is(T == GLdouble)) alias GL_DOUBLE         GL_type;
+	else static if (is(T == GLubyte )) alias GL_UNSIGNED_BYTE  GL_type;
+	else static if (is(T == GLbyte  )) alias GL_BYTE           GL_type;
+	else static if (is(T == GLushort)) alias GL_UNSIGNED_SHORT GL_type;
+	else static if (is(T == GLshort )) alias GL_SHORT          GL_type;
+	else static if (is(T == GLuint  )) alias GL_UNSIGNED_INT   GL_type;
+	else static if (is(T == GLint   )) alias GL_INT            GL_type;
+}
